57b1268dc258056bf9edecfebcc8622045c6ee16
hsh <4hyun8@gmail.com>
2021-03-11 03:08:37 +0900

    init commit

---
+++ wp-content/plugins/trx-age-gate/includes/trx-age-gate-activator.php
@@ -0,0 +1,7 @@
+<?php if (! defined('ABSPATH')) {
+    exit('No direct script access allowed');
+}
+
+class TRX_Age_Gate_Activator
+{
+}
---
+++ wp-content/plugins/trx-age-gate/trx-age-gate.php
@@ -0,0 +1,19 @@
+/**
+ * Plugin Name: TRX Age Gate
+ * Author: Team Tunaaaa Room
+ */
+
+ /*
+TRX Age Gate is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+ 
+TRX Age Gate is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with TRX Age Gate.
+*/
